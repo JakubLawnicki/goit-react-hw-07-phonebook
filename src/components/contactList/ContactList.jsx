ad640b225cd57aa30fa2396eb5490aea315a5981
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-11-28 23:11:00 +0100

    Remove useless useDispatch

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 // import { deleteContact } from 'redux/contactsSlice';
 import styles from './contactList.module.css';
 
